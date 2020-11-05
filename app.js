d08a219c6f21ab0c1fdd913b306d10e2aecee8d7
frdrcpeter007 <frdrcpeter@gmail.com>
2020-11-05 07:35:56 +0100

    Les basic de graphql

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+//Route for GraphQl
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+         
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ["Année de merde", "Hola Hola", "Mon livre de con"]
+        },
+        createEvent: (args) => {
+            return `Your name is ${args.name}`
+        }
+    },
+    graphiql: true
+}))
 
 app.listen(3000, () => {
     console.info(">>> Server started...")
